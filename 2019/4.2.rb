cba0a3a9a293666a615fe31b65f812a4e5998cdf
Chris Butler <crispygoth@gmail.com>
2019-12-04 20:09:49 +0000

    simple optimisation: swap the order of the checks as the >= test is cheaper, runtime goes from 8s to 800ms

--- 2019/4.2.rb
+++ 2019/4.2.rb
@@ -4,16 +4,16 @@
 # https://adventofcode.com/2019/day/4
 
 is_valid = lambda do |test|
-	# there must be at least one number that appears exactly twice
-	# the next check implicitly tests that they're consecutive
-	return false unless (0..9).map { |n| test.to_s.each_char.count(n.to_s) }.include? 2
-
 	test.to_s.each_char.reduce(0) do |prev, curr|
 		# all numbers must be >= the previous one
 		return false unless curr.to_i >= prev.to_i
 
 		curr
 	end
+
+	# there must be at least one number that appears exactly twice
+	# the previous check implicitly tests that they're consecutive
+	return false unless (0..9).map { |n| test.to_s.each_char.count(n.to_s) }.include? 2
 
 	true
 end
