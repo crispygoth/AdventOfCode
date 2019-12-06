a2234dd07f2c85b68cc681816c9859d7952f79d9
Chris Butler <crispygoth@gmail.com>
2019-12-06 21:34:19 +0000

    solutions for day 6

---
+++ 2019/6.2.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+#
+# Advent of Code day 2 part 1
+# https://adventofcode.com/2019/day/2
+
+require_relative 'lib/orbit_map'
+
+map = OrbitMap.new(File.new('6.input').readlines.map(&:chomp))
+puts map.find_path.length - 1
