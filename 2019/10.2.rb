26af362431e7b2f4383d2de50416f6d424daf98b
Chris Butler <crispygoth@gmail.com>
2019-12-12 19:49:50 +0000

    solutions for day 10

---
+++ 2019/10.2.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+#
+# Advent of Code day 10 part 1
+# https://adventofcode.com/2019/day/10
+
+require_relative 'lib/asteroid_field'
+
+puts AsteroidField.new(File.new('10.input').readlines.map(&:chomp)).pew_pew_pew[199]
