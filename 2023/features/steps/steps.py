efede951d5751eb9b8729416a492080ff09e03f3
Chris Butler <crispygoth@gmail.com>
2023-12-02 02:46:41 +0000

    lint fixes

--- 2023/features/steps/steps.py
+++ 2023/features/steps/steps.py
@@ -1,28 +1,33 @@
 from behave import given, when, then
 from click.testing import CliRunner
 from hamcrest import assert_that, equal_to
-from importlib import import_module
 from main import app
 import typer
 from pathlib import Path
-import os
+
 
 @given('the input data')
 def step_impl(context):
     context.input_text = context.text
 
+
 @when('the {command} {subcommand} is run')
-def step_impl(context, command, subcommand):
+def step_impl(context, command, subcommand):  # noqa:F811
     runner = CliRunner()
     with runner.isolated_filesystem():
-        Path('testinput.txt').write_text(context.input_text, newline=os.linesep)
+        Path('testinput.txt').write_text(context.input_text)
 
-        context.cli_result = runner.invoke(typer.main.get_command(app), [command, subcommand, '--input-file', 'testinput.txt'])
+        context.cli_result = runner.invoke(
+            typer.main.get_command(app),
+            [command, subcommand, '--input-file', 'testinput.txt']
+            )
+
 
 @then('the command exits successfully')
-def step_impl(context):
+def step_impl(context):  # noqa:F811
     assert_that(context.cli_result.exit_code, equal_to(0))
 
+
 @then('the result is {result}')
-def step_impl(context, result):
+def step_impl(context, result):  # noqa:F811
     assert_that(context.cli_result.stdout.strip('\n'), equal_to(result))
