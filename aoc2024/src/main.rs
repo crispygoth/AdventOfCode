79ead1dc6aa385ebcea49e6a674c4b6652e9741a
Chris Butler <crispygoth@gmail.com>
2024-12-02 20:53:46 +0000

    Rust setup + day1

---
+++ aoc2024/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+pub mod day1;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut input: String = String::new();
+
+    let mut file: File = File::open(&args[3]).expect("cannot open");
+    file.read_to_string(&mut input).expect("unable to read input file");
+
+    let result: u32;
+
+    match (&args[1][..], &args[2][..]) {
+        ("day1", "part1") => { result = day1::part1(&input); },
+        ("day1", "part2") => { result = day1::part2(&input); },
+        _ => panic!("unknown args")
+    }
+
+    println!("{}", result);
+}
