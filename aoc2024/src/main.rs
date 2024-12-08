860068021e1181bfe60dd2e71670e3b3f6d3ece8
Chris Butler <crispygoth@gmail.com>
2024-12-08 22:12:42 +0000

    2024 day 7

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -10,6 +10,7 @@ pub mod day3;
 pub mod day4;
 pub mod day5;
 pub mod day6;
+pub mod day7;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,23 +19,25 @@ fn main() {
     let mut file: File = File::open(&args[3]).expect("cannot open");
     file.read_to_string(&mut input).expect("unable to read input file");
 
-    let result: u32;
+    let result: String;
 
     let start = Instant::now();
 
     match (&args[1][..], &args[2][..]) {
-        ("day1", "part1") => { result = day1::part1(&input); },
-        ("day1", "part2") => { result = day1::part2(&input); },
-        ("day2", "part1") => { result = day2::part1(&input); },
-        ("day2", "part2") => { result = day2::part2(&input); },
-        ("day3", "part1") => { result = day3::part1(&input); },
-        ("day3", "part2") => { result = day3::part2(&input); },
-        ("day4", "part1") => { result = day4::part1(&input); },
-        ("day4", "part2") => { result = day4::part2(&input); },
-        ("day5", "part1") => { result = day5::part1(&input); },
-        ("day5", "part2") => { result = day5::part2(&input); },
-        ("day6", "part1") => { result = day6::part1(&input); },
-        ("day6", "part2") => { result = day6::part2(&input); },
+        ("day1", "part1") => { result = day1::part1(&input).to_string(); },
+        ("day1", "part2") => { result = day1::part2(&input).to_string(); },
+        ("day2", "part1") => { result = day2::part1(&input).to_string(); },
+        ("day2", "part2") => { result = day2::part2(&input).to_string(); },
+        ("day3", "part1") => { result = day3::part1(&input).to_string(); },
+        ("day3", "part2") => { result = day3::part2(&input).to_string(); },
+        ("day4", "part1") => { result = day4::part1(&input).to_string(); },
+        ("day4", "part2") => { result = day4::part2(&input).to_string(); },
+        ("day5", "part1") => { result = day5::part1(&input).to_string(); },
+        ("day5", "part2") => { result = day5::part2(&input).to_string(); },
+        ("day6", "part1") => { result = day6::part1(&input).to_string(); },
+        ("day6", "part2") => { result = day6::part2(&input).to_string(); },
+        ("day7", "part1") => { result = day7::part1(&input); },
+        ("day7", "part2") => { result = day7::part2(&input); },
         _ => panic!("unknown args")
     }
     let duration = start.elapsed();
